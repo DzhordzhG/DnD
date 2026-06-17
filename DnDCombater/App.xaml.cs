fd4fdd4c471f5f902625c722d56bbdc7fb2b27e3
enter name <enter name@gebhardt-group.com>
2026-06-17 16:35:56 +0200

    Small tweaks

--- DnDCombater/App.xaml.cs
+++ DnDCombater/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using DndCombater.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +11,15 @@ namespace DnDCombater
 	/// </summary>
 	public partial class App : Application
 	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			using (var db = new ApplicationDbContext())
+			{
+				db.Database.Migrate();
+			}
+		}
 	}
 
 }
